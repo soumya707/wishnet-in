81444cc479ea34d6f63aa60253e28ddd46c1f2f4
Synchon Mandal <synchonmandal@gmail.com>
2020-01-04 23:44:49 +0100

    refactor: add type hinting for utils.py

--- website/utils.py
+++ website/utils.py
@@ -8,6 +8,7 @@ import string
 from datetime import datetime
 from functools import wraps
 from pathlib import Path
+from typing import Callable, Dict, Sequence, Tuple
 
 import requests
 from flask import flash, redirect, session, url_for
@@ -17,14 +18,15 @@ from website.messages import (
     LOG_IN_FIRST, SUCCESSFUL_ADDPLAN, SUCCESSFUL_PROFILE_UPDATE,
     SUCCESSFUL_RECHARGE, UNSUCCESSFUL_ADDPLAN, UNSUCCESSFUL_PROFILE_UPDATE,
     UNSUCCESSFUL_RECHARGE)
+from website.mqs_api import AddPlan, Recharge, UpdateProfile
 
 
-def order_no_gen():
+def order_no_gen() -> str:
     """Generate random order numbers."""
     return ''.join(random.choices(string.ascii_letters + string.digits, k=16))
 
 
-def send_sms(url, data):
+def send_sms(url: str, data: Dict[str, str]) -> bool:
     """Send SMS to customer."""
     try:
         # avoid slow responses from server by setting a timeout of 5 seconds
@@ -40,7 +42,7 @@ def send_sms(url, data):
     return success
 
 
-def verify_mqs_topup(topup):
+def verify_mqs_topup(topup: Recharge) -> Tuple[str, str, str, str]:
     """Verify the topup status in MQS."""
     # success in MQS
     if topup.error_no == '0':
@@ -58,7 +60,7 @@ def verify_mqs_topup(topup):
     return db_entry_status, status, msg, msg_stat
 
 
-def verify_mqs_addplan(add_plan):
+def verify_mqs_addplan(add_plan: AddPlan) -> Tuple[str, str, str, str]:
     """Verify the add plan status in MQS."""
     # success in MQS
     if add_plan.error_no == '0':
@@ -76,7 +78,8 @@ def verify_mqs_addplan(add_plan):
     return db_entry_status, status, msg, msg_stat
 
 
-def verify_mqs_updateprofile(update_profile):
+def verify_mqs_updateprofile(update_profile: UpdateProfile) -> \
+    Tuple[str, str, str]:
     """Verify the update profile status in MQS."""
     # success in MQS
     if update_profile.error_no == '0':
@@ -92,7 +95,7 @@ def verify_mqs_updateprofile(update_profile):
     return db_entry_status, msg, msg_stat
 
 
-def generate_otp_secret(filepath):
+def generate_otp_secret(filepath: str) -> None:
     """Generates OTP secret key and stores it in filepath."""
     path = Path(filepath)
     # if file exists
@@ -114,7 +117,7 @@ def generate_otp_secret(filepath):
         })
 
 
-def retrieve_otp_secret(filepath):
+def retrieve_otp_secret(filepath: str) -> Dict[str, str]:
     """Retrieves OTP secret key from filepath."""
     with open(filepath, 'r') as csvfile:
         reader = csv.DictReader(csvfile)
@@ -124,7 +127,7 @@ def retrieve_otp_secret(filepath):
         }
 
 
-def get_usage(usage, offset=0, per_page=10):
+def get_usage(usage: Sequence, offset: int = 0, per_page: int = 10) -> Sequence:
     """Helper function for retrieving paginated usage information."""
     return usage[offset: offset + per_page]
 
