f167b42cdd4767fc4c9f64546f1564636640d488
Synchon Mandal <synchonmandal@gmail.com>
2020-01-08 16:02:15 +0100

    refactor: add type hinting support for paytm_utils.py

--- website/paytm_utils.py
+++ website/paytm_utils.py
@@ -2,6 +2,9 @@
 
 """Define functions for Paytm transactions."""
 
+
+from typing import Dict
+
 import requests
 
 from website import app
@@ -9,13 +12,12 @@ from website.Checksum import generate_checksum, verify_checksum
 
 
 def initiate_transaction(
-        order_id,
-        customer_no,
-        customer_mobile_no,
-        amount,
-        pay_source):
+        order_id: str,
+        customer_no: str,
+        customer_mobile_no: str,
+        amount: str,
+        pay_source: str) -> Dict[str, str]:
     """Generate checksum and form data for making transactions via Paytm."""
-
     paytm_params = {
         'MID' : app.config['PAYTM_MID'],
         'ORDER_ID' : order_id,
@@ -28,7 +30,6 @@ def initiate_transaction(
         'CALLBACK_URL' : app.config['PAYTM_CALLBACKURL'],
         'MERC_UNQ_REF': pay_source,
     }
-
     # Generate checksum for parameters we have
     paytm_params['CHECKSUMHASH'] = generate_checksum(
         paytm_params,
@@ -38,9 +39,8 @@ def initiate_transaction(
     return paytm_params
 
 
-def verify_transaction(response):
+def verify_transaction(response: Dict[str, str]) -> bool:
     """Verify the response."""
-
     paytm_params = {}
     for key, value in response.items():
         if key == 'CHECKSUMHASH':
@@ -55,22 +55,17 @@ def verify_transaction(response):
     )
 
 
-def verify_final_status(order_id):
+def verify_final_status(order_id: str) -> str:
     """Verify final status of transaction."""
-
     paytm_params = {
         'MID' : app.config['PAYTM_MID'],
         'ORDER_ID' : order_id,
     }
-
     paytm_params['CHECKSUMHASH'] = generate_checksum(
         paytm_params,
         app.config['PAYTM_MKEY']
     )
-
-    # for Production
     url = 'https://securegw.paytm.in/order/status'
-
     response = requests.post(
         url,
         json=paytm_params,
